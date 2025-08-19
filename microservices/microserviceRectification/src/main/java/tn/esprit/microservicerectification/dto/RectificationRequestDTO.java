b7f4658c7f56955e484c83d039b4c4ee85095d07
fathi <maddeh.fathi@esprit.tn>
2025-08-19 17:37:56 +0100

    Add Export import methods

---
+++ microservices/microserviceRectification/src/main/java/tn/esprit/microservicerectification/dto/RectificationRequestDTO.java
@@ -0,0 +1,14 @@
+package tn.esprit.microservicerectification.dto;
+
+import lombok.Data;
+
+@Data
+public class RectificationRequestDTO {
+    private String etudiantPrenom;
+    private String etudiantNom;
+    private String classe;
+    private String option;
+    private Double ancienneNote;
+    private Double nouvelleNote;
+    private String justification;
+}
